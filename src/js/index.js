9f12daf797eb0efa589bc2a405eee96cd0a12d1c
gzr529 <guseyn.zarbaliyev23@sitechhs.com>
2021-03-24 01:12:23 -0400

    More Small HTML and CSS
    More Small HTML and CSS

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+/* const revealButton = document.getElementById("reveal-button");
+
+revealButton.addEventListener("click", revealQuiz);
+
+function revealQuiz() {
+  console.log("Revealing...");
+}
+ */

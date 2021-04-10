c1d02f19a5e98c9ad801979691308e35b1af5ca5
ctrlLey <leyna.dinh23@sitechhs.com>
2021-04-10 01:03:06 -0400

    leaderboard + question generation
    OK SO. I added a bunch of comments to everything. I need to sort everything out. Prob gonna make a test branch bc i dont wanna mess up main. (also  bc main runs on parcel 1 instead of 2)
    The code looks way worse than it is lol. it barely does anything rn
    
    also i need to style the inputs / button

--- src/js/DOM.js
+++ src/js/DOM.js
@@ -1,11 +1,12 @@
 const DOMSelectors = {
   displayContainer: document.querySelector(".container"),
-  startButton: document.querySelector(".start-btn"),
-  questionContainer: document.querySelector(".question-container"),
+  startButton: document.querySelector("#start-btn"),
+  questionContainer: document.querySelector(".questions-container"),
   questionElement: document.querySelector(".question"),
   answerButtons: document.querySelector(".answer-buttons"),
   leaderboard: document.querySelector(".results"),
   choices: document.querySelectorAll('input[type="radio"]:checked'),
+  addName: document.forms["add-name"],
 };
 
 export { DOMSelectors };
